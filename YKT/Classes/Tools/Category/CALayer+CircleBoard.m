8c91ab6c41e3a0eec520d66e3fea232cd76b2c3d
周春仕 <350499088@qq.com>
2018-04-12 17:44:23 +0800

    no message

---
+++ YKT/Classes/Tools/Category/CALayer+CircleBoard.m
@@ -0,0 +1,19 @@
+//
+//  CALayer+CALayer_CircleBoard.m
+//  AiXiangBan
+//
+//  Created by 周春仕 on 2017/10/5.
+//  Copyright © 2017年 周春仕. All rights reserved.
+//
+
+#import "CALayer+CircleBoard.h"
+
+@implementation CALayer (CALayer_CircleBoard)
+- (void)addCircleBoardWithRadius:(CGFloat)radius boardColor:(UIColor *)boardColor boardWidth:(CGFloat)boardWidth{
+   
+    self.borderWidth = boardWidth;
+    self.cornerRadius = radius;
+    self.masksToBounds = YES;
+    
+}
+@end
