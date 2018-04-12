914d0230ac3a8c86ca6c6e5951cebc9e7fa52650
周春仕 <350499088@qq.com>
2018-04-12 16:51:01 +0800

    no message

---
+++ YKT/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YKT
+//
+//  Created by 周春仕 on 2018/4/12.
+//  Copyright © 2018年 周春仕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ YKT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YKT
+//
+//  Created by 周春仕 on 2018/4/12.
+//  Copyright © 2018年 周春仕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
